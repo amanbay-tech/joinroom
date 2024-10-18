673d7e8b2b1965d90e671adbadbe617ecd28e470
Yersain Amanbay <amanbay.tec@gmail.com>
2024-10-19 00:58:53 +0500

    lesson

--- backend/app/api/lesson.js
+++ backend/app/api/lesson.js
@@ -5,10 +5,46 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
-    res.status(200).json({
-      message: "Get Lesson list for course",
-      brands,
+    const userId = parseInt(req.body.userId, 10);
+    const courseId = parseInt(req.body.userId, 10);
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: "Bad request", message: "userId is required" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { userId } });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "User error", message: "User not found" });
+    }
+
+    const course = await prisma.myCourse.findFirst({
+      where: {
+        id: courseId,
+      },
+      include: {
+        myCourse: {
+          where: {
+            courseId,
+            status: "ALLOWED",
+          },
+        },
+      },
     });
+
+    if (!course) {
+      return res.status(200).json({ message: "No courses" });
+    }
+
+    const lesson = await prisma.lesson.findMany({ where: { id: course.id } });
+
+    if (!lesson) {
+      return res.status(200).json({ message: "No lessons" });
+    }
+    res.status(200).json({ lesson });
   } catch (error) {
     res.status(500).json({
       error: "Internal Server Error",
@@ -19,10 +55,34 @@ router.post("/", async (req, res) => {
 
 router.post("/create", async (req, res) => {
   try {
-    res.status(200).json({
-      message: "Lesson create",
-      brands,
+    const userId = parseInt(req.body.userId, 10);
+    const courseId = parseInt(req.body.userId, 10);
+    const { name, description, url, orderNumber } = req.body;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: "Bad request", message: "userId is required" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { userId } });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "User error", message: "User not found" });
+    }
+
+    const lesson = await prisma.lesson.create({
+      data: {
+        courseId,
+        orderNumber,
+        name,
+        description,
+        url,
+      },
     });
+
+    res.status(200).json({ lesson });
   } catch (error) {
     res.status(500).json({
       error: "Internal Server Error",
@@ -33,10 +93,29 @@ router.post("/create", async (req, res) => {
 
 router.post("/edit", async (req, res) => {
   try {
-    res.status(200).json({
-      message: "Lesson Edit",
-      brands,
+    const userId = parseInt(req.body.userId, 10);
+    const courseId = parseInt(req.body.userId, 10);
+    const { name, description, url } = req.body;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: "Bad request", message: "userId is required" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { userId } });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "User error", message: "User not found" });
+    }
+
+    const lesson = await prisma.lesson.update({
+      where: { courseId },
+      data: { name, description, url },
     });
+
+    res.status(200).json({ lesson });
   } catch (error) {
     res.status(500).json({
       error: "Internal Server Error",
